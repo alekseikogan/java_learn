69af49e64b77c013f5940878b66cd06bd7a3678d
alekseikogan <alekseikogan@yandex.ru>
2026-01-29 01:25:20 +0300

    балуюсь

--- first_react/src/App.js
+++ first_react/src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function Header() {
-  return <h1>Header</h1>;
+  return <h1 style={{ color: 'blue' }}>Леша</h1>;
 }
 
 function App() {
-  return <Header />;
+  const title = 'My App - and you will see it change!';
+  return (
+    <div className="App" style={{ color: 'red' }}>
+      {title} + {<Header />}
+    </div>
+  )
 }
 
 export default App;

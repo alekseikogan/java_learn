044b22762983746180cb4880fa22036ff4e92db9
Алексей <alekseikogan@yandex.ru>
2026-03-19 21:30:19 +0100

    управляемые компоненты

--- course_6/firstapp/src/App.js
+++ course_6/firstapp/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+
+  const handleSubmit = () => {
+    alert(name);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}>
+        <div className='Field'>
+          <label>Name:</label>
+          <input type='text' name='name' placeholder='Naaaaammmee' value={name} 
+          onChange={function(e) {
+            setName(e.target.value);
+          }}/>
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }

8af34b4f718e17b15aa571994bb6d142a2f99354
Алексей <alekseikogan@yandex.ru>
2026-03-28 16:48:30 +0100

    5 - feat: обновлен компонент TodoItem для использования пропсов и упрощения TodoList

--- todo_lama/src/components/TodoList.jsx
+++ todo_lama/src/components/TodoList.jsx
@@ -8,43 +8,30 @@ const TodoList = () => {
     }
 
     return (
-        <ul className="todo__list">
-            <TodoItem />
-            <li className="todo__item todo-item">
-          <input
-            className="todo-item__checkbox"
-            id="task-2"
-            type="checkbox"
-          />
-          <label
-            className="todo-item__label"
-            htmlFor="task-2"
-          >
-            Task 2
-          </label>
-          <button
-            className="todo-item__delete-button"
-            aria-label="Delete"
-            title="Delete"
-          >
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 5L5 15M5 5L15 15"
-                stroke="#757575"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
-            </li>
-        </ul>
+      <ul className="todo__list">
+        {/* задача 1 */}
+        <TodoItem 
+          className="todo__item"
+          isDone={false}
+          id="task-1"
+          title="Убраться в комнате"
+
+        />
+        {/* задача 2 */}
+        <TodoItem 
+          className="todo__item"
+          isDone={false}
+          id="task-2"
+          title="Пойти в магазин"
+        />
+        {/* задача 3 */}
+        <TodoItem 
+          className="todo__item"
+          isDone={true}
+          id="task-3"
+          title="Купить продукты"
+        />
+      </ul>
     )
 }
 

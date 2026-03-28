5b72a1a5e09a5f259aef9404aeaa88b916e95f0a
Алексей <alekseikogan@yandex.ru>
2026-03-28 13:37:31 +0100

    4 - создал Todo

--- todo_lama/src/App.jsx
+++ todo_lama/src/App.jsx
@@ -1,17 +1,8 @@
-import AddTaskForm from "./components/AddTaskForm";
-import SearchTaskForm from "./components/SearchTaskForm";
-import TodoInfo from "./components/TodoInfo";
-import TodoList from "./components/TodoList";
+import Todo from "./components/Todo";
 
 const App = () => {
   return (
-    <div className="todo">
-      <h1 className="todo__title">To Do List</h1>
-      <AddTaskForm />
-      <SearchTaskForm />
-      <TodoInfo />
-      <TodoList />
-    </div>
+    <Todo />
   )
 }
 

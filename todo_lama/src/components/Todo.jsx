f3272ad5aa66b62802131bca7acd9dae073ffec0
Алексей <alekseikogan@yandex.ru>
2026-03-28 16:58:24 +0100

    5 - feat: обновлены компоненты Todo, TodoInfo и TodoList для работы с задачами через пропсы

--- todo_lama/src/components/Todo.jsx
+++ todo_lama/src/components/Todo.jsx
@@ -5,13 +5,33 @@ import TodoList from "./TodoList";
 
 
 const Todo = () => {
+
+    const tasks = [
+        {
+            id: 1,
+            title: 'Купить хлеб',
+            isDone: false
+        },
+        {
+            id: 2,
+            title: 'Купить молоко',
+            isDone: true
+        },
+        {
+            id: 3,
+            title: 'Купить яйца',
+            isDone: false
+        }
+    ];
+
     return (
         <div className="todo">
             <h1 className="todo__title">To Do List</h1>
             <AddTaskForm />
             <SearchTaskForm />
-            <TodoInfo />
-            <TodoList />
+            <TodoInfo 
+                total={tasks.length} done={tasks.filter(task => task.isDone).length}/>
+            <TodoList tasks={tasks} />
         </div>
     )
 }
--- todo_lama/src/components/TodoList.jsx
+++ todo_lama/src/components/TodoList.jsx
@@ -1,7 +1,11 @@
 import TodoItem from "./TodoItem";
 
-const TodoList = () => {
+const TodoList = (props) => {
     const hasTasks = true; // заглушка для проверки наличия задач
+
+    const { 
+      tasks = [] 
+    } = props;
 
     if (!hasTasks) {
         return <div className="todo__empty-message"></div>;
@@ -9,28 +13,13 @@ const TodoList = () => {
 
     return (
       <ul className="todo__list">
-        {/* задача 1 */}
-        <TodoItem 
-          className="todo__item"
-          isDone={false}
-          id="task-1"
-          title="Убраться в комнате"
-
-        />
-        {/* задача 2 */}
-        <TodoItem 
-          className="todo__item"
-          isDone={false}
-          id="task-2"
-          title="Пойти в магазин"
-        />
-        {/* задача 3 */}
-        <TodoItem 
-          className="todo__item"
-          isDone={true}
-          id="task-3"
-          title="Купить продукты"
-        />
+        {tasks.map((task) => (
+          <TodoItem
+            className="todo-item"
+            key={task.id}
+            {...task}
+          />
+        ))}
       </ul>
     )
 }

2c8b8810f10d1a247d75f4a52b50439debd3f76d
Алексей <alekseikogan@yandex.ru>
2026-03-28 13:11:43 +0100

    4 - feat: добавлен компонент формы поиска задач

--- todo_lama/src/App.jsx
+++ todo_lama/src/App.jsx
@@ -1,4 +1,5 @@
 import AddTaskForm from "./components/AddTaskForm";
+import SearchTaskForm from "./components/SearchTaskForm";
 
 const App = () => {
   return (
@@ -7,23 +8,7 @@ const App = () => {
       <AddTaskForm />
 
       {/* второй филд чуть позже */}
-      <form className="todo__form">
-        <div className="todo__field field">
-          <label
-            className="field__label"
-            htmlFor="search-task"
-          >
-            Search task
-          </label>
-          <input
-            className="field__input"
-            id="search-task"
-            placeholder=" "
-            autoComplete="off"
-            type="search"
-          />
-        </div>
-      </form>
+      <SearchTaskForm />
       <div className="todo__info">
         <div className="todo__total-tasks">Total tasks: <span>0</span></div>
         <button className="todo__delete-all-button" type="button">Delete all</button>
